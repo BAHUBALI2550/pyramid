074ea4d582f784f63a01c6bd810217b5a33aa864
Himanshu Singh <hs49271.rkt@gmail.com>
2024-01-10 16:59:53 +0530

    First Push

---
+++ lib/data/exercise_options.dart
@@ -0,0 +1,50 @@
+class ExerciseOptions{
+
+
+  static List<Map<String, dynamic>> exerciseProducts = [
+    {
+      'id': '1',
+      'name': 'Belly Fat Burner',
+      'time': '10',
+      'level': 'Beginner',
+      'imageUrl': 'https://hortonbarbell.com/wp-content/uploads/2022/05/DB-Lunges.webp',
+    },
+    {
+      'id': '2',
+      'name': 'Lose Fat',
+      'time': '10',
+      'level': 'Beginner',
+      'imageUrl': 'https://hortonbarbell.com/wp-content/uploads/2022/05/DB-Lunges.webp',
+    },
+    {
+      'id': '3',
+      'name': 'Plank',
+      'time': '5',
+      'level': 'Expert',
+      'imageUrl': 'https://hortonbarbell.com/wp-content/uploads/2022/05/DB-Lunges.webp',
+    },
+    {
+      'id': '4',
+      'name': 'Build Whider',
+      'time': '30',
+      'level': 'Intermediate',
+      'imageUrl': 'https://hortonbarbell.com/wp-content/uploads/2022/05/DB-Lunges.webp',
+    },
+    {
+      'id': '5',
+      'name': 'Leg Exercises',
+      'time': '10',
+      'level': 'Beginner',
+      'imageUrl': 'https://hortonbarbell.com/wp-content/uploads/2022/05/DB-Lunges.webp',
+    },
+    {
+      'id': '6',
+      'name': 'Backward Lunges',
+      'time': '5',
+      'level': 'Beginner',
+      'imageUrl': 'https://hortonbarbell.com/wp-content/uploads/2022/05/DB-Lunges.webp',
+    },
+
+  ];
+
+}
